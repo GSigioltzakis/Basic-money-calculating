7c02fe57f7ba9a808a260d913133389743b0b617
xgr002 <82608609+xgr002@users.noreply.github.com>
2021-11-21 12:51:56 +0200

    HELLO!!!
    Use on terminal.

---
+++ Basic money calculation.py
@@ -0,0 +1,15 @@
+FPA = 0.23
+product = float(input())
+price = FPA + product
+
+print(product)
+print (price)
+
+monthly = float(price) * 12
+
+print(price)
+print(monthly)
+print("---------------------------------")
+print("The final price is " + str(price))
+print ("The FPA value is " + str(FPA))
+print("Monthly payment is " + str(monthly))
